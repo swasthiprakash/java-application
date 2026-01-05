d8a8d3615420b2244d5cd2964538b7db65c71675
swasthiprakash <swasthiprakash@gmail.com>
2026-01-05 16:05:42 +0530

    Add files via upload

--- ShoppingBill.java
+++ ShoppingBill.java
@@ -1,16 +1,11 @@
 package java_projects;
 
-import java.util.Scanner;
-
 public class ShoppingBill {
 	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Product Price:");
 		
-		double price = sc.nextDouble();
-		System.out.println("Enter quantity:");
+		double price = 200.00;
+		int qty = 5;
 		
-		int qty = sc.nextInt();
 		double total = price*qty;
 		double discount = total*0.10;
 		double finalAmount = total-discount;
